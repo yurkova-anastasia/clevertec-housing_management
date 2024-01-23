7d41f616ebaf62d26fe5eea360c9db8412bb9006
AnastYurkova <anastasia.yurkova2003@mail.ru>
2024-01-23 09:45:03 +0300

    refactor main

--- housing/src/main/java/ru/clevertec/house/HousingApplication.java
+++ housing/src/main/java/ru/clevertec/house/HousingApplication.java
@@ -2,15 +2,11 @@ package ru.clevertec.house;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.clevertec.cachestarter.aspect.CacheAspect;
 
 @SpringBootApplication
 public class HousingApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(HousingApplication.class, args);
-//        CacheAspect bean = run.getBean(CacheAspect.class);
-
+        SpringApplication.run(HousingApplication.class, args);
     }
 }
